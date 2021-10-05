c43b205710d04ca94816c1eebe8778aecac437b0
Ronan <91532347+RonanLamour@users.noreply.github.com>
2021-10-05 17:56:17 +0200

    Update exo_17.rb

--- ruby-j2/exo_17.rb
+++ ruby-j2/exo_17.rb
@@ -1,14 +1,13 @@
-
-
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
 etages = (gets.chomp).to_i
-etages.times do |i|
+if etages < 25
+then 
+  etages.times do |i|
     if i.even?
     then puts " " * (etages - i - 1) + "#" * (i * 2 + 1) + " " * (etages - i - 1 )
-    else puts " " * (etages - i - 1) + "#" * (i * 2 + 1) + " " * (etages - i - 1)      
+    else puts " " * (etages - i - 1) + "#" * (i * 2 + 1) + " " * (etages - i - 1)
     end
   end
-
-
-
+else puts "Désolé, mais il n'y a que 25 étages sur ma pyramide"
+end
